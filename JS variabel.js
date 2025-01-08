e79ac18e4d3d093026dece8d735406e804db4125
Meuthiaaulia <aulia2009fahrani@gmail.com>
2025-01-08 11:04:57 +0700

    Add files via upload

---
+++ JS variabel.js
@@ -0,0 +1,16 @@
+let nama = "Meuthia Aulia Fahrani";
+let alamat = "Karang Geneng RT.09/RW.02";
+let usia = "15 tahun";
+let ttl = "Demak, 30 Mei 2009";
+let gender = "Perempuan";
+let agama = "Islam";
+let email = "aulia2009fahrani@gmail.com";
+let berat = "60kg";
+let tinggi = "158cm";
+let hobi = "Bermain";
+let cita = "Menjadi orang sukses";
+let sts = "Pelajar";
+let sekolah = "SMK N 1 Sayung";
+let jurusan = "PPLG";
+
+document.write(" nama saya adalah " + nama + " saya tinggal di " + alamat + " pada tahun 2024 ini usia saya adalah " + usia + " tahun. Saya lahir di " + ttl + " Jenis kelamin saya adalah " + gender + " saya beragama " + agama + " alamat email saya adalah " + email + " saya memiliki berat badan " + berat + " kg dengan tinggi " + tinggi + " cm. Saya sangat senang " + hobi + " karena saya memiliki cita-cita " + cita + " saat ini saya adalah seorang " + sts + " yang bersekolah di " + sekolah + " pada jurusan " + jurusan);
